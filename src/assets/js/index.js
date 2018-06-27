0b98ef8530d2aa38892a3f807407c1f4c9ff9420
ecitlm <ecitlm@163.com>
2018-06-27 15:21:22 +0800

    update

--- src/assets/js/index.js
+++ src/assets/js/index.js
@@ -1,3 +1,13 @@
 /* global  */
 import '../css/common.scss'
 import './news.js'
+
+console.log(process.env)
+if (process.env.NODE_ENV === 'development') {
+}
+
+if (process.env.NODE_ENV !== 'production') {
+  const VConsole = require('vconsole')
+  const vConsole = new VConsole()
+  console.log(vConsole)
+}
